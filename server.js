1c58d7aa3fbe9edeb4c8461b0f82fb4b609a879f
KayPea <prescottdarling@gmail.com>
2021-07-08 16:41:51 -0400

    defined path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const Application = require("./models/Application");
 
